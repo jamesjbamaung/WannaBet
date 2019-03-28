779b145abe1027cfa30a1e393a85257d9f3a0d6a
jjbamaung <jamesjbamaung@gmail.com>
2019-03-28 12:01:34 -0500

    updated UserMessage delete feature

--- Models/UserMessage.cs
+++ Models/UserMessage.cs
@@ -10,6 +10,8 @@ public class UserMessage
     public string Info { get; set; }
     public int SenderId { get; set; }
     public int ReceiverId { get; set;}
+    public bool SenderDelete { get; set; }
+    public bool ReceiverDelete { get; set; }
     public User Sender { get; set; }
     public User Receiver { get; set; }
     public DateTime CreatedAt {get;set;} = DateTime.Now;
